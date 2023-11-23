3ded168ce736f360b55a2d91e1658033632ef493
Shahrzad <75183431+shahrzadblackfox@users.noreply.github.com>
2023-11-23 21:42:51 +0330

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@
 function App() {
   return (
     <div className="section-center">
+      <h4>لورم ایپسوم ساز</h4>
+      <form className="lorem0">
+        <label htmlFor="total">تعداد پاراگراف</label>
+        <input type="number" />
+        <button type="submit">ساختن</button>
+
+
+      </form>
+      <article className="lorem-text">
+        <p>
+
+        </p>
+
+      </article>
 
     </div>
   );
